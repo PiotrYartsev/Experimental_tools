b5c4685bd9ebebd15aaea522d1c7b96fcc768c89
Piotr Yartsev <piotr_1997@live.se>
2021-12-16 09:11:24 +0100

    solved A

---
+++ open_lab_digital_A.py
@@ -0,0 +1,7 @@
+print("A=0, B=0, QNAND=1, QNAND_inverted=0")
+
+print("A=0, B=1, QNAND=1, QNAND_inverted=0")
+
+print("A=1, B=0, QNAND=1, QNAND_inverted=0")
+
+print("A=1, B=1, QNAND=0, QNAND_inverted=1")
