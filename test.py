11ba95beb9b616358942a33ac4519bb1314b8bd4
Piotr Yartsev <piotr_1997@live.se>
2022-03-15 11:37:27 +0100

    asdas

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
 #find out when file was created
-from rucio.clients import Client
+from rucio.client import Client
+from rucio.common.exception import RucioException
+from rucio.common.utils import generate_uuid
 
-
-Client=Client()
-
-#for each file in the current directory
-
+import sys
+import os
 import time
 import sys
 
+CLIENT = Client()
 
+print(CLIENT.ping())
+
+#print(CLIENT.get_metadata(scope="mc20", name="mc_v9-8GeV-1e-inclusive_1000_t1588350689.root"))
+os.system("cd; cd rucio-client-venv/bin; source activate")
+L=os.popen("rucio get-metadata mc20:mc_v9-8GeV-1e-inclusive_1000_t1588350689.root").read()
+
+L2=L.split("\n")
+L2=[s.strip('\n') for s in L2]
+L3=[s.split(":") for s in L2]
+
+print(L3)
